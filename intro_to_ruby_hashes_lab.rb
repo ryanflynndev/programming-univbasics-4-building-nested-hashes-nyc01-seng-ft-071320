0ba4fadb71b56698f166dbf455763a60c04fbf5f
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-06 20:53:13 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,9 +34,7 @@ def monopoly_with_third_tier
       :four_pieces_owned => 200
     },
     :names => {
-      :reading_railroad => {
-        "mortgage_value"=>"$100"
-      },
+      :reading_railroad => {},
       :pennsylvania_railroad => {},
       :b_and_o_railroad => {},
       :shortline_railroad => {}
@@ -55,17 +53,31 @@ def monopoly_with_fourth_tier
   {:railroads => 
   {
     :pieces => 4,
-    :rent_in_dollars => {
+    :rent_in_dollars => 
+    {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
       :three_pieces_owned => 100,
       :four_pieces_owned => 200
     },
-    :names => {
-      :reading_railroad => {},
-      :pennsylvania_railroad => {},
-      :b_and_o_railroad => {},
-      :shortline_railroad => {}
+    :names => 
+    {
+      :reading_railroad => 
+      {
+        "mortgage_value"=>"$100"
+      },
+      :pennsylvania_railroad => 
+      {
+        
+      },
+      :b_and_o_railroad => 
+      {
+        
+      },
+      :shortline_railroad => 
+      {
+        
+      }
       
     }
   }
